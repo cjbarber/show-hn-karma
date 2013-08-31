21f45001c848b6900dce77b87515bbde0155f13b
Chris Barber <c.barber80@gmail.com>
2013-08-31 16:37:34 -0700

    pretty much ready

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,33 +1,5 @@
-if (Commenters.find().count() === 0) {
-  // Commenters.insert({
-  //   username: 'cjbarber',
-  //   project: {
-  //     body: '<a href="http://google.com">My cool project</a>',
-  //     feedback_received: 5
-  //   },
-  //   feedback_given: 5
-  // });
+var MyCron = new Cron();
 
-  // Commenters.insert({
-  //   username: 'eric',
-  //   project: {
-  //     body: 'hey im eric',
-  //     feedback_received: 9
-  //   },
-  //   feedback_given: 2
-  // });
-
-  // Commenters.insert({
-  //   username: 'jojo',
-  //   project: {
-  //     body: 'hey im jojo',
-  //     feedback_received: 9
-  //   },
-  //   feedback_given: 1
-  // });
-
-  // Commenters.insert({
-  //   username: 'toonice',
-  //   feedback_given: 1
-  // });
-}
+MyCron.addJob(30, function() {
+  Meteor.call('loadCommenters');
+});
