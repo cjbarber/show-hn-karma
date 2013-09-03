d8d9fc2023f2c829dede65d0a342fbcbd9a60865
Chris Barber <c.barber80@gmail.com>
2013-09-03 11:22:16 -0700

    Added working cron task

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,5 +1,52 @@
-// var MyCron = new Cron();
+// Veeeeeeery simple cron job singleton
+// ticks every 1 minute, set a job to go every X ticks.
 
-// MyCron.addJob(30, function() {
-//   Meteor.call('loadCommenters');
-// });
+Cron = function(interval) {
+  var self = this;
+  
+  interval = interval || 60 * 1000;
+  
+  self._jobs = [];
+  self._schedules = [];
+
+  Meteor.setInterval(function() {
+    self.tick();
+  }, interval)
+}
+
+_.extend(Cron.prototype, {
+  addJob: function(every_x_ticks, fn) {
+    this._jobs.push({fn: fn, every: every_x_ticks, count: 0});
+  },
+
+  addScheduleJob: function(unix_time, fn) {
+    this._schedules.push({fn: fn, unix_time: unix_time});
+  },
+  
+  tick: function() {
+    var self = this;
+    
+    _.each(self._jobs, function(job) {
+      job.count += 1;
+      if (job.count === job.every) {
+        job.fn();
+        job.count = 0;
+      }
+    });
+
+    _.each(self._schedules, function(job, index) {
+      var ts = Math.round((new Date()).getTime() / 1000);
+
+      if (ts >= job.unix_time) {
+        job.fn();
+        delete self._schedules[index];
+      }
+    });
+  }
+})
+
+var MyCron = new Cron();
+
+MyCron.addJob(10, function() {
+  Meteor.call('loadCommenters');
+});
